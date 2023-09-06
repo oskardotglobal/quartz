34235b21af139b58848b3575792373ab14bad39d
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2023-09-06 16:49:54 +0200

    feat: abuse explicit filter plugin for own uses

--- quartz/plugins/filters/explicit.ts
+++ quartz/plugins/filters/explicit.ts
@@ -3,7 +3,9 @@ import { QuartzFilterPlugin } from "../types"
 export const ExplicitPublish: QuartzFilterPlugin = () => ({
   name: "ExplicitPublish",
   shouldPublish(_ctx, [_tree, vfile]) {
+    if (!(vfile.dirname === "Pandora's Box/Projekte" || vfile.dirname === "Pandora's Box/Blog")) return true;
+    
     const publishFlag: boolean = vfile.data?.frontmatter?.publish ?? false
-    return publishFlag
+    return publishFlag 
   },
 })
