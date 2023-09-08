ba2c2de19bfe17cc45fc2d756e318f2957ef5e14
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2023-09-08 17:15:07 +0200

    fix: explicit filter filters regardless of vault name

--- quartz/plugins/filters/explicit.ts
+++ quartz/plugins/filters/explicit.ts
@@ -3,7 +3,7 @@ import { QuartzFilterPlugin } from "../types"
 export const ExplicitPublish: QuartzFilterPlugin = () => ({
   name: "ExplicitPublish",
   shouldPublish(_ctx, [_tree, vfile]) {
-    if (vfile.dirname !== "Pandora's Box/Blog") return true;
+    if (vfile.dirname?.split("/").at(1) !== "Blog") return true;
     
     const publishFlag: boolean = vfile.data?.frontmatter?.publish ?? false
     return publishFlag 
