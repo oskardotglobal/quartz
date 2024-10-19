6f50cdb67122c09c579652d5d278b223f112fa42
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-10-19 19:56:44 +0200

    feat: unfuck filtering.. again

--- quartz/plugins/filters/explicit.ts
+++ quartz/plugins/filters/explicit.ts
@@ -3,7 +3,7 @@ import { QuartzFilterPlugin } from "../types"
 export const ExplicitPublish: QuartzFilterPlugin = () => ({
   name: "ExplicitPublish",
   shouldPublish(_ctx, [_tree, vfile]) {
-    const path = `/${vfile.data?.relativePath?.split("/").at(0)}`
-    return !(path in ["/Blog", "/"]) || vfile.data?.frontmatter?.publish === true
+    const path = `/${vfile.data?.slug?.split("/").at(0)}`
+    return (path !== "/Blog" && path !== "/") || vfile.data?.frontmatter?.publish === "true"
   },
 })
